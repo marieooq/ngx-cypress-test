b906355a3b675f5c0b676f802510685fb630f05a
Marie Otaki <otakimarie@gmail.com>
2020-12-04 12:51:46 -0800

    add a test case to understand the difference between click and check

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -162,7 +162,7 @@ describe("Our first suite", () => {
       });
   });
 
-  it.only("radio button", () => {
+  it("radio button", () => {
     cy.visit("/");
     cy.contains("Form").click();
     cy.contains("Form Layout").click();
@@ -182,5 +182,15 @@ describe("Our first suite", () => {
 
         cy.wrap(radioButtons).eq(2).should("be.disabled");
       });
+  });
+
+  it.only("check boxes", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Toastr").click();
+
+    // cy.get('[type="checkbox"]').check({ force: true });
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
+    cy.get('[type="checkbox"]').eq(1).check({ force: true });
   });
 });
